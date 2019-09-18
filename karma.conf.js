97f3032ac98be6e22c88d4c2d371e3c32f4d452b
BaryRas <gentimechant73@gmail.com>
2019-09-18 17:20:24 +0200

    feat (ep3): add ci ep 1
    - add Travis
    - add config chrome (Travis & Protractor)

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,8 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -25,15 +27,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chromium', 'Chrome', 'Chrome_without_security'],
+    browsers: ['ChromeHeadless', 'Chrome'],
       customLaunchers: {
-        Chrome_without_security: {
+        ChromeHeadlessCI: {
           base: 'ChromeHeadless',
-          flags: ['--no-sandbox', '--disable-gpu', '--disable-web-security', '--disable-site-isolation-trials']
+          flags: ['--no-sandbox', '--disable-gpu']
         }
       },
     singleRun: false,
     restartOnFileChange: true
   });
 };
-
